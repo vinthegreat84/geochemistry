2cc9b7ab12f85a3be2819dec2869875604850849
Vinay Babu <59340330+vinthegreat84@users.noreply.github.com>
2022-01-30 11:09:44 +0530

    Add files via upload
    Section 'Home' updated for trivariate plot.

--- cws/cws.py
+++ cws/cws.py
@@ -35,6 +35,7 @@ import io
 
 # to do
 # spider plot
+# histogram and to check the distribution type
 ##############################################################################################################
 # home
 def home():
@@ -47,10 +48,11 @@ def home():
     st.write('\n')
     st.write('### Sections')
     st.write('**Data panel:** Selection of the major elemental geochemical dataset.')
-    st.write('**Variation diagram:** Variation of major oxides against samples.')
     st.write('**Data filter:** Data filter based on sample/category/subcategory/subsubcategory.')
+    st.write('**Major Oxides variation**: Variation of major oxides against samples.')
     st.write('**Weathering proxy:** Data table of chemical weathering indices including Chemical Index of Weathering (CIW) after [**Harnois, 1988**](https://doi.org/10.1016/0037-0738(88)90137-6); Chemical Proxy of Alteration (CPA) after [**Buggle et al., 2011**](https://doi.org/10.1016/j.quaint.2010.07.019); Chemical Index of Alteration (CIA) after [**Nesbitt and Young, 1982**](https://doi.org/10.1038/299715a0); Plagioclase Index of Alteration (PIA) after [**Fedo et al., 1995**](https://doi.org/10.1130/0091-7613(1995)023<0921:UTEOPM>2.3.CO;2); Modified Chemical Index of Alteration (CIX) after [**Garzanti et al., 2014**](https://doi.org/10.1016/j.chemgeo.2013.12.016); Index of Compositional Variability (ICV) after [**Cox et al., 1995**](https://doi.org/10.1016/0016-7037(95)00185-9); Weathering Index of Parker (WIP) after [**Parker, 1970**](https://doi.org/10.1017/S0016756800058581) and chemical proxies like SiO2/Al2O3, K2O/Al2O3, Al2O3/TiO2.')
-    st.write('**Bivariate plot:** Bivariate plot between oxide and/or weathering index with variable-based marker size and linear/non-linear trendline and axes.')
+    st.write('**Bivariate plot:** Bivariate plot between oxide and/or weathering index with variable-based marker size, linear/non-linear trendline & axes, and marginal distribution.')
+    st.write('**Trivariate plot:** Trivariate plot between oxide and/or weathering index with variable-based marker size and linear/non-linear trendline & axes.')
     st.write('**Compositional space diagram:** Compositional space diagrams including A - CN - K compositional space diagram after [**Nesbitt and Young, 1982**](https://doi.org/10.1038/299715a0); A - CNK - FM compositional space diagram after [**Nesbitt and Young, 1989**](https://doi.org/10.1086/629290) and M - F - W compositional space diagram after [**Ohta and Arai, 2007**](https://doi.org/10.1016/j.chemgeo.2007.02.017).')
     st.write('**Boxplot, Scatter matrix, Correlation matrix and Heatmap:** Boxplot, Scatter matrix, Correlation matrix and Heatmap of chemical weathering indices and proxies.')
         
@@ -152,7 +154,7 @@ def data_analysis():
             subsubcat = st.sidebar.selectbox("Select the Subsubcatgory of the data filter:", data['subsubcategory'].unique())
             data_subsubcat = data[data['subsubcategory'].isin([subsubcat])]
             data = data_subsubcat              
-
+        
     # Major oxides variation
     if st.sidebar.checkbox('Major oxides variation'):
         st.header('Major oxides variation')
