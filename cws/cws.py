8c07058dc3b79726eeab876da1bec62cb4bbd447
Vinay Babu <59340330+vinthegreat84@users.noreply.github.com>
2022-02-08 18:42:22 +0530

    Add files via upload
    plt excluded.

--- cws/cws.py
+++ cws/cws.py
@@ -14,7 +14,7 @@ from plotly.subplots import make_subplots
 # from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 # import plotly.figure_factory as ff
 # import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 # import altair as alt
 import io
 # from io import BytesIO
